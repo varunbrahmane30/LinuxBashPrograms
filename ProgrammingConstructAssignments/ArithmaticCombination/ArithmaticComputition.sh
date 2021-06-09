a7bb5ba232c0e0eeaf087d1056f483772ee6216b
name <varunbrahmane30@gmail.com>
2021-06-09 18:33:10 +0530

    Compute (a+b*c)

--- ProgrammingConstructAssignments/ArithmaticCombination/ArithmaticComputition.sh
+++ ProgrammingConstructAssignments/ArithmaticCombination/ArithmaticComputition.sh
@@ -1,13 +1,19 @@
 #!/bin/bash -x
 
 
-read -p "Enter a value :: "  a 
+read -p "Enter a value :: "  a
 
 read -p "Enter b value :: "  b
 
-read -p "Enter c value :: "  c 
+read -p "Enter c value :: "  c
 
 
 echo " a value is : " $a
 echo " b value is : " $b
 echo " c value is : " $c
+
+
+
+ result1=$(( $a+$b*$c ))
+
+echo $result1;
