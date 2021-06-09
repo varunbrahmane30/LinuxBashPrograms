0ea226d120362261292bab401e44c060187b9c82
name <varunbrahmane30@gmail.com>
2021-06-09 18:13:22 +0530

    No of times won

--- ProgrammingConstructAssignments/FlipcoinProb1/FlipCoinSimulator.sh
+++ ProgrammingConstructAssignments/FlipcoinProb1/FlipCoinSimulator.sh
@@ -1,11 +1,21 @@
 #!/bin/bash -x
 
+hcount=0
+tcount=0
 
-randomCheck=$(( RANDOM%2 ))
+while [[ ($hcount -lt 21) && ($tcount -lt 21) ]]
+do
+	randomCheck=$(( RANDOM%2 ))
+	if [[ $randomCheck == 0 ]]
+	then
+		echo "Head"
+		((hcount++));
+	else
+		echo "Tail"
+		((tcount++));
+	fi
+done
 
-if [ $randomCheck -eq 1 ]
-then 
-		echo Tail
-else
-		echo Head
-fi
+echo "head count is: $hcount"
+echo "tail count is: $tcount"
+
