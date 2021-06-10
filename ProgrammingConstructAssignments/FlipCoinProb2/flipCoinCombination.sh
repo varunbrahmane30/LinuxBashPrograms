bf8cc634fe813264d4f3446d3cc5d6e29c1014e7
name <varunbrahmane30@gmail.com>
2021-06-10 11:04:59 +0530

    Calculate Doublet

--- ProgrammingConstructAssignments/FlipCoinProb2/flipCoinCombination.sh
+++ ProgrammingConstructAssignments/FlipCoinProb2/flipCoinCombination.sh
@@ -1,16 +1,42 @@
 #!/bin/bash -x
 
 scount=0
+dcount=0
 
 randomcheck=$((RANDOM%2))
 
+while [[ $res1 -le 20 && $res2 -le 20 ]]
+do
+	randomcheck=$((RANDOM%2))
 if [[ $randomcheck -eq 0 ]]
-then 
-	echo H
-	res1=$(( scount++ ))
-else
-	echo T
-	res1=$(( scount++ ))
-fi
+then
+	echo H;
+	res1=$((scount++))
+	randomcheck=$((RANDOM%2))
 
-echo $scount
+	if [[ $randomcheck -eq 1 ]]
+	then
+		echo HT;
+		res2=$((dcount++))
+	else 
+		echo HH;
+		res2=$((dcount++))
+	fi
+else
+	echo T;
+	res1=$((scount++))
+	randomcheck=$((RANDOM%2))
+	if [[ $randomcheck -eq 0 ]]
+	then
+		echo TT;
+		res2=$((dcount++))
+	else 
+		echo TH;
+		res2=$((dcount++))
+	fi
+fi
+ 
+done
+
+echo $res1
+echo $res2
