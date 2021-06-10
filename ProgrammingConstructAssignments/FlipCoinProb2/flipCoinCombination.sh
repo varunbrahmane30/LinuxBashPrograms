227162531efd3bf09afd6649df40602226b17385
name <varunbrahmane30@gmail.com>
2021-06-10 09:50:26 +0530

    Singlet count

--- ProgrammingConstructAssignments/FlipCoinProb2/flipCoinCombination.sh
+++ ProgrammingConstructAssignments/FlipCoinProb2/flipCoinCombination.sh
@@ -1,10 +1,16 @@
 #!/bin/bash -x
 
+scount=0
+
 randomcheck=$((RANDOM%2))
 
 if [[ $randomcheck -eq 0 ]]
 then 
 	echo H
+	res1=$(( scount++ ))
 else
 	echo T
+	res1=$(( scount++ ))
 fi
+
+echo $scount
